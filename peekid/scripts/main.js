31871b9456e875cac20507106f4446d8ad731dcb
onion1007 <85293570+onion1007@users.noreply.github.com>
2021-06-03 23:42:00 +0800

    Add files via upload

---
+++ peekid/scripts/main.js
@@ -0,0 +1,17 @@
+var the_h3 = document.querySelector('h3');
+
+function func1(){
+  the_h3.innerHTML = "你的描述:其實還好";
+}
+function func2(){
+  the_h3.innerHTML = "你的描述:嗯?";
+}
+function func3(){
+  the_h3.innerHTML = "你的描述:開始變糟";
+}
+function func4(){
+  the_h3.innerHTML = "你的描述:使人厭惡";
+}
+function func5(){
+  the_h3.innerHTML = "你的描述:可以被公開撻伐了";
+}
